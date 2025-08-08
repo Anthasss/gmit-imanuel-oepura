71c57763202dd922c86b98a797310103200a286e
Anthasss <anthasprog@gmail.com>
2025-08-08 08:44:10 +0800

    added home header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1>Home</h1>
+    <div>
+      <div className="flex justify-start items-center h-screen">
+        <img
+          src="/header/home.jpg"
+          alt="Home Head"
+          className="object-cover w-full h-full"
+        />
+        <div className="absolute flex flex-col p-16">
+          <p className="text-white text-4xl font-bold">Welcome to</p>
+          <h1 className="text-white text-6xl font-bold">GMIT Imanuel Oepura</h1>
+          <p>Together in love, growing in faith, serving in hope.</p>
+
+        </div>
+      </div>
+
     </div>
   );
 }
